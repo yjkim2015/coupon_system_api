a1c2f8ca2f17424755cbaae85eb2d7048b6af052
gmarket\ykim14 <yjkim14@gmarket.com>
2024-09-21 14:56:56 +0900

    redis 쿠폰 정합성 처리

---
+++ src/main/java/com/gmarket/coupon_system_api/repository/CouponCountRepository.java
@@ -0,0 +1,23 @@
+package com.gmarket.coupon_system_api.repository;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author : ykim14
+ * @fileName : CouponCountRepository
+ * @since : 2024-09-21
+ */
+
+@Repository
+public class CouponCountRepository {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public CouponCountRepository(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Long increment() {
+        return redisTemplate.opsForValue().increment("coupon_count");
+    }
+}
--- src/main/java/com/gmarket/coupon_system_api/service/ApplyService.java
+++ src/main/java/com/gmarket/coupon_system_api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.gmarket.coupon_system_api.service;
 
 import com.gmarket.coupon_system_api.domain.Coupon;
+import com.gmarket.coupon_system_api.repository.CouponCountRepository;
 import com.gmarket.coupon_system_api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +16,17 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    private final CouponCountRepository couponCountRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     public void apply(Long userId) {
-        long count = couponRepository.count();
+
+        //long count = couponRepository.count();
+        Long count = couponCountRepository.increment();
         if (count > 100) {
             return;
         }
